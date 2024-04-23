185f71e312180e9ee0118dbc60e9c8dc33ee0785
Goolski <andrzej.golski1@gmail.com>
2024-04-23 20:50:21 +0200

    Refactor ChatManagerProvider constructor in chat_manager_provider.dart

--- lib/features/basic_chat_support/chat_manager_provider.dart
+++ lib/features/basic_chat_support/chat_manager_provider.dart
@@ -4,11 +4,11 @@ import 'package:char_creator/features/basic_chat_support/chat_manager.dart';
 class ChatManagerProvider extends InheritedWidget {
   final ChatManager chatManager;
 
-  ChatManagerProvider({
-    Key? key,
+  const ChatManagerProvider({
+    super.key,
     required this.chatManager,
-    required Widget child,
-  }) : super(key: key, child: child);
+    required super.child,
+  });
 
   static ChatManagerProvider? of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<ChatManagerProvider>();
