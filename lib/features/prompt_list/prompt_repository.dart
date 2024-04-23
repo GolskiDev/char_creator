4ae52db60ccd0a00509c0d2d962257856dd19f7c
Goolski <andrzej.golski1@gmail.com>
2024-04-23 21:28:20 +0200

    Refactor PromptDataSource get method

--- lib/features/prompt_list/prompt_repository.dart
+++ lib/features/prompt_list/prompt_repository.dart
@@ -4,19 +4,21 @@ class PromptDataSource {
   Future<List<PromptModel>> get({
     List<String> ids = const [],
   }) {
-    return Future.value([
-      PromptModel(
-        prompt: 'What is your name?',
-        id: '1',
-      ),
-      PromptModel(
-        prompt: 'What is your favorite color?',
-        id: '2',
-      ),
-      PromptModel(
-        prompt: 'What is your favorite food?',
-        id: '3',
-      ),
-    ]);
+    return Future.value(
+      [
+        PromptModel(
+          prompt: 'What is your name?',
+          id: '1',
+        ),
+        PromptModel(
+          prompt: 'What is your favorite color?',
+          id: '2',
+        ),
+        PromptModel(
+          prompt: 'What is your favorite food?',
+          id: '3',
+        ),
+      ],
+    );
   }
 }
