3181c2a6cbe407a356250443066b584e42bca5ea
Goolski <andrzej.golski1@gmail.com>
2024-04-29 21:21:18 +0200

    Refactor padding values in JsonObjectWidget for improved UI consistency

--- lib/features/styling/main_theme.dart
+++ lib/features/styling/main_theme.dart
@@ -2,4 +2,12 @@ import 'package:flutter/material.dart';
 
 final ThemeData mainTheme = ThemeData(
   useMaterial3: true,
+  colorScheme: ColorScheme.fromSwatch(
+    brightness: Brightness.light,
+    primarySwatch: Colors.brown,
+    cardColor: Colors.brown.shade100,
+    backgroundColor: Colors.brown.shade50,
+    accentColor: Colors.brown.shade300,
+    errorColor: Colors.red.shade400,
+  ),
 );
