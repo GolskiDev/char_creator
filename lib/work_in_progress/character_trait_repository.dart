5c8675e7e82b7493f477ec6e1080d3fd031f8ba1
Goolski <andrzej.golski1@gmail.com>
2024-07-26 19:05:53 +0200

    Deleted comments

--- lib/work_in_progress/character_trait_repository.dart
+++ lib/work_in_progress/character_trait_repository.dart
@@ -2,26 +2,21 @@ import 'dart:convert';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'character_trait.dart'; // Import your CharacterTrait model
+import 'character_trait.dart';
 
 class CharacterTraitRepository {
   static const String _storageKey = 'characterTraits';
 
-  // Serialize a CharacterTrait object to a JSON string
   String _encodeTrait(CharacterTrait trait) {
-    final Map<String, dynamic> traitMap =
-        trait.toJson(); // Assuming you have a toJson method
+    final Map<String, dynamic> traitMap = trait.toJson();
     return json.encode(traitMap);
   }
 
-  // Deserialize a JSON string to a CharacterTrait object
   CharacterTrait _decodeTrait(String encodedTrait) {
     final Map<String, dynamic> traitMap = json.decode(encodedTrait);
-    return CharacterTrait.fromJson(
-        traitMap); // Assuming you have a fromJson constructor
+    return CharacterTrait.fromJson(traitMap);
   }
 
-  // Save a new character trait
   Future<void> saveTrait(CharacterTrait trait) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String encodedTrait = _encodeTrait(trait);
@@ -30,7 +25,6 @@ class CharacterTraitRepository {
     await prefs.setStringList(_storageKey, traits);
   }
 
-  // Retrieve all character traits
   Future<List<CharacterTrait>> getAllTraits() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final List<String>? encodedTraits = prefs.getStringList(_storageKey);
@@ -42,8 +36,6 @@ class CharacterTraitRepository {
         .toList();
   }
 
-  // Example: Implement more CRUD operations as needed
-  // Update an existing character trait
   Future<void> updateTrait(CharacterTrait updatedTrait) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String> traits = prefs.getStringList(_storageKey) ?? [];
@@ -55,7 +47,6 @@ class CharacterTraitRepository {
     }
   }
 
-// Delete a character trait
   Future<void> deleteTrait(String traitId) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String> traits = prefs.getStringList(_storageKey) ?? [];
