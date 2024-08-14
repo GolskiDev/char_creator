6b7ffbd4f5e2a0bd0e15dc2b52a6f021b625b421
Goolski <andrzej.golski1@gmail.com>
2024-08-14 16:55:34 +0200

    Added EditStringPage to CharacterTraitPage

--- lib/work_in_progress/views/character_trait_page.dart
+++ lib/work_in_progress/views/character_trait_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../character_trait.dart';
 import '../character_trait_repository.dart';
+import 'edit_trait_page.dart';
 
 class CharacterTraitPage extends HookConsumerWidget {
   const CharacterTraitPage({
@@ -14,17 +14,23 @@ class CharacterTraitPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isEdit = useState(false);
-    final textController = useTextEditingController(text: trait.value);
-
     final displayView = Scaffold(
       appBar: AppBar(
         title: Text(trait.id),
         actions: [
           IconButton(
             icon: const Icon(Icons.edit),
-            onPressed: () {
-              isEdit.value = true;
+            onPressed: () async {
+              final newValue = await Navigator.of(context).push<String>(
+                MaterialPageRoute<String>(
+                  builder: (context) {
+                    return EditStringPage(initialValue: trait.value);
+                  },
+                ),
+              );
+              if (newValue != null) {
+                _updateTrait(trait, newValue);
+              }
             },
           ),
         ],
@@ -35,28 +41,7 @@ class CharacterTraitPage extends HookConsumerWidget {
       ),
     );
 
-    final editView = Scaffold(
-      appBar: AppBar(
-        title: Text(trait.id),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.save),
-            onPressed: () {
-              isEdit.value = false;
-              _updateTrait(trait, textController.text);
-            },
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextField(
-          controller: textController,
-        ),
-      ),
-    );
-
-    return isEdit.value ? editView : displayView;
+    return displayView;
   }
 
   _updateTrait(SingleValueCharacterTrait trait, String newValue) async {
