531177af47e95143b265cd296317a8c488e1bd40
Goolski <andrzej.golski1@gmail.com>
2024-10-31 19:34:15 +0100

    Added Drag and drop

--- lib/work_in_progress/character/widgets/character_page.dart
+++ lib/work_in_progress/character/widgets/character_page.dart
@@ -58,6 +58,16 @@ class CharacterPage extends HookConsumerWidget {
     WidgetRef ref,
     Character character,
   ) {
+    final characterFieldWidgets = character.fields
+        .map(
+          (field) => _characterField(
+            context,
+            ref,
+            character,
+            field,
+          ),
+        )
+        .toList();
     return Scaffold(
       appBar: AppBar(),
       floatingActionButton: _floatingActionButton(
@@ -68,17 +78,8 @@ class CharacterPage extends HookConsumerWidget {
       endDrawer: ChatPage(
         character: character,
       ),
-      body: ListView.builder(
-        itemCount: character.fields.length,
-        itemBuilder: (context, index) {
-          final field = character.fields[index];
-          return _characterField(
-            context,
-            ref,
-            character,
-            field,
-          );
-        },
+      body: ListView(
+        children: characterFieldWidgets,
       ),
     );
   }
@@ -89,73 +90,159 @@ class CharacterPage extends HookConsumerWidget {
     Character character,
     Field field,
   ) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                field.name,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    final notesWidgets = field.notes.map(
+      (note) {
+        return _noteCard(
+          context,
+          ref,
+          character,
+          field,
+          note,
+        );
+      },
+    ).toList();
+    return DragTarget<Note>(
+      onAcceptWithDetails: (details) {
+        // move note from one field to another
+        final note = details.data;
+        // check Where the note is coming from
+        final fromField = character.fields.firstWhereOrNull(
+          (f) => f.notes.contains(note),
+        );
+        // check Where the note is going to
+        final toField = character.fields.firstWhereOrNull(
+          (f) => f == field,
+        );
+        if (fromField != null && toField != null) {
+          final updatedFromField = fromField.copyWith(
+            notes: fromField.notes.where((n) => n != note).toList(),
+          );
+          final updatedToField = toField.copyWith(
+            notes: [...toField.notes, note],
+          );
+          final updatedFields = character.fields
+              .map((f) => f == fromField
+                  ? updatedFromField
+                  : f == toField
+                      ? updatedToField
+                      : f)
+              .toList();
+          final updatedCharacter = character.copyWith(
+            fields: updatedFields,
+          );
+          ref.read(characterRepositoryProvider).updateCharacter(
+                updatedCharacter,
+              );
+        }
+      },
+      builder: (context, candidateItems, rejectedItems) {
+        final fieldName = Text(
+          field.name,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        );
+        final newNoteButton = IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => NoteFormPage(
+                  onSavePressed: (context, ref, note) {
+                    final updatedField = field.copyWith(
+                      notes: [...field.notes, Note.create(value: note.value)],
+                    );
+                    final updatedFields = character.fields
+                        .map((f) => f == field ? updatedField : f)
+                        .toList();
+                    final updatedCharacter = character.copyWith(
+                      fields: updatedFields,
+                    );
+                    ref.read(characterRepositoryProvider).updateCharacter(
+                          updatedCharacter,
+                        );
+                  },
+                ),
               ),
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => NoteFormPage(
-                        onSavePressed: (context, ref, note) {
-                          final updatedField = field.copyWith(
-                            notes: [
-                              ...field.notes,
-                              Note.create(value: note.value)
-                            ],
-                          );
-                          final updatedFields = character.fields
-                              .map((f) => f == field ? updatedField : f)
-                              .toList();
-                          final updatedCharacter = character.copyWith(
-                            fields: updatedFields,
-                          );
-                          ref.read(characterRepositoryProvider).updateCharacter(
-                                updatedCharacter,
-                              );
-                        },
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ],
+            );
+          },
+        );
+        final listOfFields = ScrollConfiguration(
+          behavior: const ScrollBehavior().copyWith(
+            dragDevices: {
+              PointerDeviceKind.touch,
+              PointerDeviceKind.mouse,
+            },
           ),
-        ),
-        ScrollConfiguration(
-          behavior: const ScrollBehavior().copyWith(dragDevices: {
-            PointerDeviceKind.touch,
-            PointerDeviceKind.mouse,
-          }),
           child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
-              children: field.notes
-                  .map(
-                    (note) => _noteCard(
-                      context,
-                      ref,
-                      character,
-                      field,
-                      note,
-                    ),
-                  )
-                  .toList(),
+              children: notesWidgets,
             ),
           ),
-        ),
-        SizedBox(height: 12),
-      ],
+        );
+        if (candidateItems.isNotEmpty) {
+          return Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        fieldName,
+                        Visibility(
+                          visible: false,
+                          maintainSize: true,
+                          maintainAnimation: true,
+                          maintainState: true,
+                          child: newNoteButton,
+                        ),
+                      ],
+                    ),
+                  ),
+                  listOfFields,
+                ],
+              ),
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  color: Theme.of(context).primaryColor.withOpacity(0.5),
+                  child: const Center(
+                    child: Text(
+                      'Drop here',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        }
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  fieldName,
+                  newNoteButton,
+                ],
+              ),
+            ),
+            listOfFields,
+          ],
+        );
+      },
     );
   }
 
@@ -166,51 +253,15 @@ class CharacterPage extends HookConsumerWidget {
     Field field,
     Note note,
   ) {
-    return Card(
+    final child = Card(
       child: InkWell(
         borderRadius: BorderRadius.circular(8),
         onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => NoteFormPage(
-                initialValue: TraitFormState(
-                  value: note.value,
-                  tags: [],
-                ),
-                onSavePressed: (context, ref, updatedNote) {
-                  final updatedField = field.copyWith(
-                    notes: field.notes
-                        .map((n) => n == note
-                            ? Note.create(value: updatedNote.value)
-                            : n)
-                        .toList(),
-                  );
-                  final updatedFields = character.fields
-                      .map((f) => f == field ? updatedField : f)
-                      .toList();
-                  final updatedCharacter = character.copyWith(
-                    fields: updatedFields,
-                  );
-                  ref.read(characterRepositoryProvider).updateCharacter(
-                        updatedCharacter,
-                      );
-                },
-                onDeletePressed: (context, ref) {
-                  final updatedField = field.copyWith(
-                    notes: field.notes.where((n) => n != note).toList(),
-                  );
-                  final updatedFields = character.fields
-                      .map((f) => f == field ? updatedField : f)
-                      .toList();
-                  final updatedCharacter = character.copyWith(
-                    fields: updatedFields,
-                  );
-                  ref.read(characterRepositoryProvider).updateCharacter(
-                        updatedCharacter,
-                      );
-                },
-              ),
-            ),
+          _onNotePressed(
+            context,
+            note,
+            field,
+            character,
           );
         },
         child: Padding(
@@ -221,6 +272,61 @@ class CharacterPage extends HookConsumerWidget {
               Text(note.value),
             ],
           ),
+        ),
+      ),
+    );
+    return LongPressDraggable<Note>(
+      delay: const Duration(milliseconds: 200),
+      data: note,
+      feedback: child,
+      childWhenDragging: Opacity(
+        opacity: 0.5,
+        child: child,
+      ),
+      child: child,
+    );
+  }
+
+  void _onNotePressed(
+      BuildContext context, Note note, Field field, Character character) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => NoteFormPage(
+          initialValue: TraitFormState(
+            value: note.value,
+            tags: [],
+          ),
+          onSavePressed: (context, ref, updatedNote) {
+            final updatedField = field.copyWith(
+              notes: field.notes
+                  .map((n) =>
+                      n == note ? Note.create(value: updatedNote.value) : n)
+                  .toList(),
+            );
+            final updatedFields = character.fields
+                .map((f) => f == field ? updatedField : f)
+                .toList();
+            final updatedCharacter = character.copyWith(
+              fields: updatedFields,
+            );
+            ref.read(characterRepositoryProvider).updateCharacter(
+                  updatedCharacter,
+                );
+          },
+          onDeletePressed: (context, ref) {
+            final updatedField = field.copyWith(
+              notes: field.notes.where((n) => n != note).toList(),
+            );
+            final updatedFields = character.fields
+                .map((f) => f == field ? updatedField : f)
+                .toList();
+            final updatedCharacter = character.copyWith(
+              fields: updatedFields,
+            );
+            ref.read(characterRepositoryProvider).updateCharacter(
+                  updatedCharacter,
+                );
+          },
         ),
       ),
     );
