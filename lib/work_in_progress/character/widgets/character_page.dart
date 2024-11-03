a96cb12d6183c2d21dadf7958999a260e04ee94d
Goolski <andrzej.golski1@gmail.com>
2024-11-03 19:58:31 +0100

    Removed unused imports

--- lib/work_in_progress/character/widgets/character_page.dart
+++ lib/work_in_progress/character/widgets/character_page.dart
@@ -1,5 +1,4 @@
 import 'package:collection/collection.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
