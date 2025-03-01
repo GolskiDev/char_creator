7b3c17c7d483440d434b94ff3e3f001b14e6a09e
Goolski <andrzej.golski1@gmail.com>
2025-03-01 20:13:26 +0100

    Started updating ListOfSpells design

--- lib/features/spells/list_of_spells_page.dart
+++ lib/features/spells/list_of_spells_page.dart
@@ -1,35 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'open5e/open_5e_spells_repository.dart';
+import 'widgets/spell_filter_drawer.dart';
 
 class ListOfSpellsPage extends HookConsumerWidget {
   const ListOfSpellsPage({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final allCantrips = ref.watch(allSRDCantripsProvider);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Spells'),
+    final menuEntries = [
+      MenuItemButton(
+        child: const Text('Hello'),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Hello'),
+            ),
+          );
+        },
       ),
-      body: allCantrips.when(
-        data: (cantrips) {
-          return ListView.separated(
-            itemCount: cantrips.length,
-            itemBuilder: (context, index) {
-              final cantrip = cantrips[index];
-              return ListTile(
-                title: Text(cantrip.name),
-                onTap: () {
-                  context.go('/spells/${cantrip.name}');
+      MenuItemButton(
+        child: const Text('There'),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('There'),
+            ),
+          );
+        },
+      ),
+    ];
+
+    final searchController = useTextEditingController();
+    final searchFocusNode = useFocusNode();
+
+    final appBarTitle = Builder(
+      builder: (context) {
+        if (searchFocusNode.hasFocus || searchController.text.isNotEmpty) {
+          return TextField(
+            focusNode: searchFocusNode,
+            controller: searchController,
+            onTapOutside: (_) {
+              searchController.clear();
+            },
+            decoration: InputDecoration(
+              hintText: 'Search',
+              //clear
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () {
+                  searchController.clear();
+                  searchFocusNode.unfocus();
+                },
+              ),
+            ),
+          );
+        }
+
+        return const Text('Cantrips');
+      },
+    );
+
+    return Scaffold(
+      endDrawer: const SpellFilterDrawer(),
+      appBar: AppBar(
+        title: appBarTitle,
+        actions: [
+          MenuAnchor(
+            menuChildren: menuEntries,
+            child: IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {
+                //Open menu
+              },
+            ),
+            builder: (context, controller, child) {
+              return IconButton(
+                icon: const Icon(Icons.more_vert),
+                onPressed: () {
+                  controller.isOpen ? controller.close() : controller.open();
                 },
               );
             },
-            separatorBuilder: (context, index) {
-              return const Divider();
+          ),
+          Builder(
+            builder: (context) {
+              return IconButton(
+                icon: const Icon(Icons.tune),
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
+                },
+              );
             },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FocusScope.of(context).requestFocus(searchFocusNode);
+        },
+        child: const Icon(Icons.search),
+      ),
+      body: allCantrips.when(
+        data: (cantrips) {
+          return SafeArea(
+            child: Stack(
+              children: [
+                ListView.separated(
+                  itemCount: cantrips.length,
+                  itemBuilder: (context, index) {
+                    final cantrip = cantrips[index];
+                    return ListTile(
+                      title: Text(cantrip.name),
+                      onTap: () {
+                        context.go('/spells/${cantrip.name}');
+                      },
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return const Divider();
+                  },
+                ),
+              ],
+            ),
           );
         },
         loading: () => const Center(
---
+++ lib/features/spells/widgets/spell_filter_drawer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SpellFilterDrawer extends StatelessWidget {
+  const SpellFilterDrawer({super.key,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(),
+    );
+  }
+}
