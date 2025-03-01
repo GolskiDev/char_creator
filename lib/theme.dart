ca2ec6ad3844a757491cf339ffbf156099cf916c
Goolski <andrzej.golski1@gmail.com>
2025-03-01 22:21:39 +0100

    Added dark theme

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Colors2 {
+class LightColorScheme {
   final richBrown = const Color(0xFF6D4C41);
   final mutedOliveGreen = const Color(0xFF556B2F);
   final burgundy = const Color(0xFFA52A2A);
   final burntOrange = const Color(0xFFD2691E);
   final darkGrey = const Color(0xFF2F4F4F);
   final beige3 = const Color.fromARGB(255, 248, 236, 210);
+
+  ColorScheme get colorScheme => ColorScheme.fromSeed(
+          dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
+          seedColor: LightColorScheme().burgundy,
+          secondary: LightColorScheme().burntOrange,
+          tertiary: LightColorScheme().mutedOliveGreen,
+          surface: LightColorScheme().beige3,
+        );
+}
+
+class DarkColorScheme {
+  ColorScheme get colorScheme => ColorScheme.fromSeed(
+          dynamicSchemeVariant: DynamicSchemeVariant.fruitSalad,
+          brightness: Brightness.dark,
+          seedColor: Color.fromARGB(255, 235, 187, 55),
+          surface: Color.fromARGB(255, 15, 15, 15), 
+        );
 }
 
 class AppTheme {
@@ -15,13 +32,7 @@ class AppTheme {
   double get borderRadius => 8;
   get themeData => ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          dynamicSchemeVariant: DynamicSchemeVariant.fidelity,
-          seedColor: Colors2().burgundy,
-          secondary: Colors2().burntOrange,
-          tertiary: Colors2().mutedOliveGreen,
-          surface: Colors2().beige3,
-        ),
+        colorScheme: DarkColorScheme().colorScheme,
         fontFamily: font.fontFamily,
         cardTheme: CardTheme(
           elevation: 0,
