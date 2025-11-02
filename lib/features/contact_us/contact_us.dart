99dfd486771a0983bab02bd33da6b2b44dfc1b5c
Goolski <andrzej.golski1@gmail.com>
2025-11-02 13:26:11 +0100

    Contact us - dynamic version name and updated email

--- lib/features/contact_us/contact_us.dart
+++ lib/features/contact_us/contact_us.dart
@@ -1,18 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+import '../app_version/app_version_interactor.dart';
 // Remember to configure UrlLauncher as in pub.dev
 
 class ContactUs {
-  static sendMail() {
-    final mail = "support@example.com";
+  static Future<void> sendMail(BuildContext context, WidgetRef ref) async {
+    final version = await ref.watch(appVersionStateProvider.future);
+    final versionString = version.currentVersion.toString();
+    return _sendMail(versionName: versionString);
+  }
+
+  static Future<void> _sendMail({
+    required String versionName,
+  }) async {
+    final mail = "contact@golski.dev";
     final title = "App Support";
     final body = "Please write your message here";
-    final footer = "\n\n--\nAppName v1.0.0";
+    final footer = "\n\n--\Spells & Tools v$versionName";
     final url = Uri(
       scheme: 'mailto',
       path: mail,
       query: 'subject=$title&body=$body$footer',
     );
-    launchUrl(
+    await launchUrl(
       url,
     );
   }
