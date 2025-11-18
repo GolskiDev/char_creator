30285995b20ff850eeeb74da3ff03150dfe58c31
Goolski <andrzej.golski1@gmail.com>
2025-11-18 23:22:00 +0100

    Fixed contact us

--- lib/features/app_version/app_version_interactor.dart
+++ lib/features/app_version/app_version_interactor.dart
@@ -1,23 +1,36 @@
 import 'package:async/async.dart';
 import 'package:riverpod/riverpod.dart';
+import 'package:spells_and_tools/features/app_version/models/app_version_representation.dart';
 
 import 'app_version_state.dart';
 import 'sources/current_app_version_source.dart';
 import 'sources/remote_app_version_source.dart';
 import 'sources/update_link_source.dart';
 
-final appVersionStateProvider = StreamProvider<AppVersionState>(
+final appVersionStateProvider = StreamProvider<UpdateVersionState>(
   (ref) {
     final interactor = ref.watch(appVersionInteractorProvider);
     return interactor.getAppVersionStateStream();
   },
 );
 
+final currentAppVersionProvider = FutureProvider<AppVersionRepresentation>(
+  (ref) {
+    final interactor = ref.watch(appVersionInteractorProvider);
+    return interactor.currentAppVersion;
+  },
+);
+
+final currentAppVersionSourceProvider = Provider(
+  (ref) => CurrentAppVersionSource(),
+);
+
 final appVersionInteractorProvider = Provider(
   (ref) {
     final updateLinkSource = ref.watch(updateLinkSourceProvider);
-    return AppVersionInteractor(
-      CurrentAppVersionSource(),
+    final currentAppVersionSource = ref.watch(currentAppVersionSourceProvider);
+    return AppUpdateInteractor(
+      currentAppVersionSource,
       RemoteAppVersionSource(
         remoteLatestVersionStream: Stream.empty(),
         remoteRequiredVersionStream: Stream.empty(),
@@ -27,18 +40,22 @@ final appVersionInteractorProvider = Provider(
   },
 );
 
-class AppVersionInteractor {
+class AppUpdateInteractor {
   final CurrentAppVersionSource currentSource;
   final RemoteAppVersionSource remoteSource;
   final UpdateLinkSource updateLinkSource;
 
-  AppVersionInteractor(
+  AppUpdateInteractor(
     this.currentSource,
     this.remoteSource,
     this.updateLinkSource,
   );
 
-  Stream<AppVersionState> getAppVersionStateStream() async* {
+  Future<AppVersionRepresentation> get currentAppVersion {
+    return currentSource.getCurrentVersion();
+  }
+
+  Stream<UpdateVersionState> getAppVersionStateStream() async* {
     final currentVersion = await currentSource.getCurrentVersion();
 
     final zippedStream = StreamZip([
@@ -50,7 +67,7 @@ class AppVersionInteractor {
       final latestVersion = versions[0];
       final latestRequiredVersion = versions[1];
 
-      yield AppVersionState(
+      yield UpdateVersionState(
         currentVersion: currentVersion,
         latestVersion: latestVersion,
         latestRequiredVersion: latestRequiredVersion,
--- lib/features/app_version/app_version_state.dart
+++ lib/features/app_version/app_version_state.dart
@@ -1,17 +1,18 @@
 import 'models/app_version_representation.dart';
 
-class AppVersionState {
+class UpdateVersionState {
   final AppVersionRepresentation currentVersion;
   final AppVersionRepresentation? latestVersion;
   final AppVersionRepresentation? latestRequiredVersion;
 
-  AppVersionState({
+  UpdateVersionState({
     required this.currentVersion,
     required this.latestVersion,
     required this.latestRequiredVersion,
   });
 
-  bool get isUpdateAvailable => (latestVersion?.compareTo(currentVersion) ?? -1) > 0;
+  bool get isUpdateAvailable =>
+      (latestVersion?.compareTo(currentVersion) ?? -1) > 0;
   bool get isForceUpdateRequired =>
       (latestRequiredVersion?.compareTo(currentVersion) ?? -1) > 0;
 }
