08348b2850b639cedd2e36579ea8930167c458f2
Goolski <andrzej.golski1@gmail.com>
2025-11-23 21:03:18 +0100

    Fixed displaying tos again after registering

--- lib/features/onboarding/pages/update_agreements_dialog.dart
+++ lib/features/onboarding/pages/update_agreements_dialog.dart
@@ -1,56 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:spells_and_tools/features/navigation/dialog_route.dart';
 
-import '../../terms/data_sources/agreements_documents_data_source.dart';
 import '../../terms/terms_of_service_interactor.dart';
 import '../../terms/widgets/terms_and_conditions_widget.dart';
 
 class UpdateAgreementsDialog {
   static DialogPage route({
     required BuildContext context,
-    required AgreementDetails? termsOfUseDetails,
-    required AgreementDetails? privacyPolicyDetails,
-  }) =>
-      DialogPage(
-        builder: (context) {
-          final title = 'Terms and Conditions Update';
-          final subtitle =
-              'We have updated our Terms and Conditions and Privacy Policy. Please review and accept the updated documents to continue using the app.';
-          return AlertDialog(
-            title: Text(
-              title,
-              textAlign: TextAlign.center,
-            ),
-            content: SingleChildScrollView(
-              child: Column(
-                spacing: 8,
-                children: [
-                  Text(
-                    subtitle,
-                    textAlign: TextAlign.center,
-                  ),
-                  AgreementsWidget(
-                    termsOfUseDetails: termsOfUseDetails,
-                    privacyPolicyDetails: privacyPolicyDetails,
-                    onContinue: (context, ref) async {
-                      await ref
-                          .read(
-                            updatedAgreementsSubmitterProvider.notifier,
-                          )
-                          .submitAgreements(
-                            privacyPolicy: privacyPolicyDetails,
-                            termsOfUse: termsOfUseDetails,
-                          );
-                      if (context.mounted) {
-                        context.pop();
-                      }
-                    },
-                  ),
-                ],
+  }) {
+    return DialogPage(
+      barrierDismissible: false,
+      builder: (context) {
+        final title = 'Terms and Conditions Update';
+        final subtitle =
+            'We have updated our Terms and Conditions and Privacy Policy. Please review and accept the updated documents to continue using the app.';
+        return Consumer(
+          builder: (context, ref, child) {
+            final termsOfUseDetails =
+                ref.watch(requiredUpdatedAgreementsProvider).asData?.value.termsOfUse;
+            final privacyPolicyDetails =
+                ref.watch(requiredUpdatedAgreementsProvider).asData?.value.privacyPolicy;
+            return AlertDialog(
+              title: Text(
+                title,
+                textAlign: TextAlign.center,
               ),
-            ),
-          );
-        },
-      );
+              content: SingleChildScrollView(
+                child: Column(
+                  spacing: 8,
+                  children: [
+                    Text(
+                      subtitle,
+                      textAlign: TextAlign.center,
+                    ),
+                    AgreementsWidget(
+                      termsOfUseDetails: termsOfUseDetails,
+                      privacyPolicyDetails: privacyPolicyDetails,
+                      onContinue: (context, ref) async {
+                        await ref
+                            .read(
+                              updatedAgreementsSubmitterProvider.notifier,
+                            )
+                            .submitAgreements(
+                              privacyPolicy: privacyPolicyDetails,
+                              termsOfUse: termsOfUseDetails,
+                            );
+                        if (context.mounted) {
+                          context.pop();
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
 }
